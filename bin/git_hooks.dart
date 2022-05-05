b190335e4ab78e569d343226fc2b80c3e857710f
liwenbo <liwenbo@supermonkey.com.cn>
2022-05-05 10:44:38 +0800

    fix: test exit code

--- bin/git_hooks.dart
+++ bin/git_hooks.dart
@@ -1,10 +1,14 @@
-import 'package:dart_git_hooks/dart_git_hooks.dart';
-import 'dart:io';
 import 'dart:core';
+import 'dart:io';
+
+import 'package:dart_git_hooks/dart_git_hooks.dart';
 
 void main(List<String> arguments) {
   // ignore: omit_local_variable_types
-  Map<Git, UserBackFun> params = {Git.commitMsg: commitMsg, Git.preCommit: preCommit};
+  Map<Git, UserBackFun> params = {
+    Git.commitMsg: commitMsg,
+    Git.preCommit: preCommit
+  };
   GitHooks.call(arguments, params);
 }
 
@@ -20,19 +24,22 @@ Future<bool> commitMsg() async {
       commitMsg.startsWith('chore:')) {
     return true; // you can return true let commit go
   } else {
-    print('commit message 格式： `fix:`, `feat:`, `docs:`, `style:`, `refactor:`, `test:`, `chore:`, \n 参考地址：https://www.ruanyifeng.com/blog/2016/01/commit_message_change_log.html');
+    print(
+      'commit message 格式： `fix:`, `feat:`, `docs:`, `style:`, `refactor:`, `test:`, `chore:`, '
+      '\n 参考地址：https://www.ruanyifeng.com/blog/2016/01/commit_message_change_log.html',
+    );
     return false;
   }
 }
 
 Future<bool> preCommit() async {
-
   try {
     print('dart analyze 开始分析... ${DateTime.now()}');
     //--options analysis_options.yaml lib
     ProcessResult result = await Process.run('dart', ['analyze']);
     print('dart analyze 分析中... ${result.stdout}');
     print("dart analyze 分析完成 - ${DateTime.now()} \t 返回码：${result.exitCode}");
+    print('dart analyze err: ${result.stderr}');
     if (result.exitCode != 0) return false;
   } catch (e) {
     print('错误：$e');
---
+++ bin/util/date_fmt.dart
@@ -0,0 +1,12 @@
+/// 日期格式化
+class DateFmt {
+  static String durationIndicatorBuilder(Duration duration) {
+    const String separator = ':';
+    final String minute = duration.inMinutes.toString().padLeft(2, '0');
+    final String second =
+        ((duration - Duration(minutes: duration.inMinutes)).inSeconds)
+            .toString()
+            .padLeft(2, '0');
+    return '$minute$separator$second';
+  }
+}
