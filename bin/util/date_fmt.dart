86428aaa0f400798e0b6e97cdf56a9b253c2d428
liwenbo <liwenbo@supermonkey.com.cn>
2022-05-05 10:47:49 +0800

    fix: test linter exit code

--- bin/util/date_fmt.dart
+++ bin/util/date_fmt.dart
@@ -1,9 +1,14 @@
+import 'dart:io' as IO;
+
 /// 日期格式化
 class DateFmt {
   static String durationIndicatorBuilder(Duration duration) {
-    const String separator = ':';
-    final String minute = duration.inMinutes.toString().padLeft(2, '0');
-    final String second =
+    IO.HttpDate date = IO.HttpDate();
+    print('date = $date');
+
+    const separator = ':';
+    final minute = duration.inMinutes.toString().padLeft(2, '0');
+    final second =
         ((duration - Duration(minutes: duration.inMinutes)).inSeconds)
             .toString()
             .padLeft(2, '0');
